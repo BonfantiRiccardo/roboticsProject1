68cc320fbb84356223483c57b7b6056d4ac7838f
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-18 10:29:34 +0200

    cose ma non capisco

--- src/gps_to_odom.cpp
+++ src/gps_to_odom.cpp
@@ -17,28 +17,26 @@ private:
 
 	ros::Timer timer;			//si può togliere se  facciamo publishing a hertz fissati (controllare se il subscriber legge a hertz fissati con comando)
 
-	double reference_latitude = 0;
-	double reference_longitude = 0;
-	double reference_altitude = 0;
+	double reference_latitude = 45;
+	double reference_longitude = 9;
+	double reference_altitude = 169;
 
 	double toRadians(double degrees) {
 		return degrees * M_PI / 180;
 	}
 
-	double* toECEF(double latitude, double longitude, double altitude) {
+	Eigen::Vector3d toECEF(double latitude, double longitude, double altitude) {
 		double a = 6378137.0;
 		double b = 6356752.0;
-		double eUp2 = 1 - (pow(b, 2))/(pow(a, 2));
+		double eUp2 = 1 - ((pow(b, 2))/(pow(a, 2)));
 		double nConstant = a / sqrt(1 - eUp2 * pow(sin(latitude), 2));
 
 		double x = (nConstant + altitude) * cos(latitude) * cos(longitude);
 		double y = (nConstant + altitude) * cos(latitude) * sin(longitude);
 		double z = (nConstant * (1 - eUp2) + altitude) * sin(latitude);
 
-		double* ECEF = new double[3];
-		ECEF[0] = x;
-		ECEF[1] = y;
-		ECEF[2] = z;
+		Eigen::Vector3d ECEF;
+		ECEF << x, y, z;
 
 		return ECEF;
 	}
@@ -58,25 +56,24 @@ public:
 		double longitude = toRadians(msg->longitude);
 		double altitude = toRadians(msg->altitude);
 		
-		double* ECEF = toECEF(latitude, longitude, altitude);
+		Eigen::Vector3d ECEF = toECEF(latitude, longitude, altitude);
 
-		double* ECEF_reference = toECEF(reference_latitude, reference_longitude, reference_altitude);
+		reference_latitude = toRadians(reference_latitude);
+		reference_longitude = toRadians(reference_longitude);
+		reference_altitude = toRadians(reference_altitude);
+
+		Eigen::Vector3d ECEF_reference = toECEF(reference_latitude, reference_longitude, reference_altitude);
 
 		// to ENU
-		double x = ECEF[0] - ECEF_reference[0];
-		double y = ECEF[1] - ECEF_reference[1];
-		double z = ECEF[2] - ECEF_reference[2];
+		Eigen::Vector3d vector1 = ECEF - ECEF_reference;
 
-		Eigen::MatrixXd matrix1(3, 1);
-		matrix1 << x, y, z;
-
-		Eigen::MatrixXd matrix2(3, 3);
+		Eigen::Matrix3d matrix2;
 		matrix2 << 
 		-sin(reference_longitude), cos(reference_longitude), 0,
 		-sin(reference_latitude) * cos(reference_longitude), -sin(reference_latitude) * sin(reference_longitude), cos(reference_latitude),
-		-cos(reference_latitude) * cos(reference_longitude), -cos(reference_latitude) * sin(reference_longitude), -sin(reference_latitude);
+		cos(reference_latitude) * cos(reference_longitude), cos(reference_latitude) * sin(reference_longitude), sin(reference_latitude);
 
-		Eigen::MatrixXd ENU = matrix2 * matrix1;
+		Eigen::MatrixXd ENU = matrix2 * vector1;
 
 		messaggio.pose.pose.position.x = ENU(0, 0);
 		messaggio.pose.pose.position.y = ENU(1, 0);
