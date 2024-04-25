03dbbd8da49d91768a5036f0b625bb2ff3d3cd14
EdoardoBergamoMac <edoardo.bergamo@mail.polimi.it>
2024-04-25 23:54:43 +0200

    corrected typos that didn't make it compile

--- src/lidar_viz.cpp
+++ src/lidar_viz.cpp
@@ -11,22 +11,18 @@ private:
 	ros::NodeHandle nh;
 	ros::Subscriber sub;
 	ros::Publisher pub;
-
-	//Maybe it has to be initialized
-	String currentFrame;
-
 	
 
 public:
 
-	pub_sub(){
+	//Maybe it has to be initialized
+	std::string currentFrame;
 
-		f = boost::bind(&callback, _1, _2);
-  		server.setCallback(f);
+	pub_sub(){
 
   		dynamic_reconfigure::Server<first_project::parametersConfig> server;
     	dynamic_reconfigure::Server<first_project::parametersConfig>::CallbackType f;
-  		f = boost::bind(&callback1, this, _1, _2);
+  		f = boost::bind(&pub_sub::callback1, this, _1, _2);
   		server.setCallback(f);
 
 
@@ -38,17 +34,15 @@ public:
 
 	void callback(const sensor_msgs::PointCloud2::ConstPtr& msg){
 		ROS_INFO("FRAME: %s", msg->header.frame_id.c_str());
+		currentFrame = msg->header.frame_id.c_str();
 		//TODO
 	}
 
 	void callback1(first_project::parametersConfig &config, uint32_t level){
 		ROS_INFO("Reconficure request: %s", config.frame_id.c_str());
-		currentFrame = config.frame_id.c_str();
-
 	}
 
 };
-
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "lidar_remap");
